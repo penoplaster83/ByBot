0d363daef457d07051037f5e99d4347785727dad
penoplaster83 <penoplaster83@gmail.com>
2024-04-14 13:26:18 +0600

    1st Orders and Balance ops

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ You can skip authentication by not passing any value for api_key
 and api_secret.
 """
 session = HTTP(
-    testnet=True,
+    testnet=False,
     api_key="MWvHnQQH3kgWD32o6P",
     api_secret="gByweE1l74shCdBxCte8lX66QgDZh6r7L0aL",
 )
 
-response = session.get_orderbook(category = "linear", symbol = "BTC")
+response = session.get_orderbook(category = "linear", symbol = "BTCUSDT")
+print(session.get_wallet_balance(accountType = "UNIFIED"))
 
 print (response)
 
